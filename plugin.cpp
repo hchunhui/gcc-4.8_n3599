ab6ea18c19f2df232ac81e0035fcb8eed76c77d1
Chunhui He <hchunhui@mail.ustc.edu.cn>
2021-05-05 07:36:28 +0000

    add notice

--- plugin.cpp
+++ plugin.cpp
@@ -5,15 +5,27 @@
 #include "gcc-plugin.h"
 #include "tree.h"
 #include "langhooks.h"
+#include "cpplib.h"
+#include "c-family/c-common.h"
+#include "diagnostic-core.h"
+
 int plugin_is_GPL_compatible;
 
 void new_c_parse_file (void);
 void init_pragma (void);
 
+extern struct cpp_options *cpp_opts;
+
 void start_unit_callback(void *gcc_data, void *user_data)
 {
 	if (strcmp(lang_hooks.name, "GNU C++") == 0) {
-		fprintf(stderr, "install new C++ parser\n");
+		if (cxx_dialect < cxx1y) {
+			verbatim("notice: use \"-std=c++1y\" to turn on n3599");
+		}
+
+		if (!cpp_opts->preprocessed) {
+			fatal_error("rerun with \"-no-integrated-cpp\"");
+		}
 		init_pragma();
 		lang_hooks.parse_file = new_c_parse_file;
 	}
